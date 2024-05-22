c5f2fa63f43d53148e2667a550182a93e6e48684
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-21 20:50:32 -0500

    improve

--- test/encodeSpecialCharacters.js
+++ test/encodeSpecialCharacters.js
@@ -1,18 +1,27 @@
-const bookmarkleter = require( '../bookmarkleter' );
+const bookmarkleter = require('../bookmarkleter');
 
-exports.encodeSpecialCharacters = test => {
-  const data = [
-    [
-      'var test = \'%"<>#@&?\';',
-      'javascript:var%20test=%22%25\\%22%3C%3E%23%40%26%3F%22;',
+exports.encodeSpecialCharacters = () => {
+  const testCases = [
+    ['Test case 1: Encoding special characters in a string',
+      'var test = ''%"<>#@&?';,
+      'javascript:var%20test=%22%25\\%22%3C%3E%23%40%26%3F%22;'
     ],
-    [
+    ['Test case 2: Encoding special characters in an internationalized string',
       'var test = \'Iñtërnâtiônàlizætiøn\';',
-      'javascript:var%20test=%22I\\xF1t\\xEBrn\\xE2ti\\xF4n\\xE0liz\\xE6ti\\xF8n%22;',
+      'javascript:var%20test=%22I\\xF1t\\xEBrn\\xE2ti\\xF4n\\xE0liz\\xE6ti\\xF8n%22;'
     ],
   ];
 
-  data.forEach( ( [ input, output ] ) => test.equal( bookmarkleter( input ), output ) );
+  testCases.forEach(([description, input, expectedOutput]) => {
+    test(description, () => {
+      const output = bookmarkleter(input);
+      expect(output).toEqual(expectedOutput);
+    });
+  });
 
-  test.done();
+  // If you are using Jest, you can use `test.each` instead of `forEach`
+  // test.each(testCases)(description, (input, expectedOutput) => {
+  //   const output = bookmarkleter(input);
+  //   expect(output).toEqual(expectedOutput);
+  // });
 };
