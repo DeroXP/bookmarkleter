c2749a03e7924105a77829ecfb6b176161373b3a
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-21 20:50:32 -0500

    improve

--- test/injectJQuery.js
+++ test/injectJQuery.js
@@ -1,25 +1,31 @@
-const bookmarkleter = require( '../bookmarkleter' );
+const bookmarkleter = require('../bookmarkleter');
+
+if (!bookmarkleter) {
+  throw new Error('Failed to require bookmarkleter module');
+}
 
 exports.injectJQuery = test => {
-  const hasJQuery = ( input, options = {} ) => bookmarkleter( input, options ).includes( 'loadBookmarklet' );
+  /**
+   * Checks if the output of bookmarkleter contains 'loadBookmarklet'
+   * @param {string} input - The input string to pass to bookmarkleter
+   * @param {object} options - Options to pass to bookmarkleter
+   * @returns {boolean} True if 'loadBookmarklet' is in the output, false otherwise
+   */
+  const hasJQuery = (input, options = {}) =>
+    bookmarkleter(input, options).includes('loadBookmarklet');
 
-  var data = [
-    'var test;',
-    'console.log("test");',
-  ];
+  const data = ['var test;', 'console.log("test");'];
 
-  // jQuery: true
-  data.forEach( input => test.ok( hasJQuery( input, { jQuery: true } ) ) );
+  test.comment('Testing with jQuery: true');
+  data.forEach(input => test.ok(hasJQuery(input, { jQuery: true }), 'should include loadBookmarklet'));
 
-  // jQuery: false (default)
-  data.forEach( input => test.ok( ! hasJQuery( input, { jQuery: false } ) ) );
-  data.forEach( input => test.ok( ! hasJQuery( input ) ) );
+  test.comment('Testing with jQuery: false (default) and not specified');
+  data.forEach(input => test.ok(!hasJQuery(input, { jQuery: false }), 'should not include loadBookmarklet'));
+  data.forEach(input => test.ok(!hasJQuery(input), 'should not include loadBookmarklet'));
 
-  // jquery: true (legacy option)
-  data.forEach( input => test.ok( hasJQuery( input, { jquery: true } ) ) );
-
-  // jquery: false (legacy option)
-  data.forEach( input => test.ok( ! hasJQuery( input, { jquery: false } ) ) );
+  test.comment('Testing with jquery: true and false (legacy options)');
+  data.forEach(input => test.ok(hasJQuery(input, { jquery: true }), 'should include loadBookmarklet'));
+  data.forEach(input => test.ok(!hasJQuery(input, { jquery: false }), 'should not include loadBookmarklet'));
 
   test.done();
 };
