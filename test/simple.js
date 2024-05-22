2022daa29d60caf87f4edcf44a925da7f82c6b0c
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-21 20:50:30 -0500

    improve

--- test/simple.js
+++ test/simple.js
@@ -1,38 +1,17 @@
-const bookmarkleter = require( '../bookmarkleter' );
+const bookmarkleter = require('../bookmarkleter');
 
-exports.simple = test => {
+exports.simple = (test) => {
   const data = [
-    [
-      'test',
-      'javascript:test;',
-    ],
-    [
-      'var test',
-      'javascript:var%20test;',
-    ],
-    [
-      'var test = function(){var foo};',
-      'javascript:var%20test=function(){var%20foo};',
-    ],
-    [
-      'var test = function(foo){foo++};',
-      'javascript:var%20test=function(foo){foo++};',
-    ],
-    [
-      'document.write("bookmarklet");',
-      'javascript:document.write(%22bookmarklet%22);',
-    ],
-    [
-      'window.open("http://www.google.com");',
-      'javascript:window.open(%22http://www.google.com%22);',
-    ],
-    [
-      'const getMessage = () => "Hello World";',
-      'javascript:const%20getMessage=()=%3E%22Hello%20World%22;',
-    ],
+    ['test', 'javascript:test;'],
+    ['var test', 'javascript:var%20test;'],
+    ['var test = function(){var foo};', 'javascript:var%20test=function(){var%20foo};'],
+    ['var test = function(foo){foo++};', 'javascript:var%20test=function(foo){foo++};'],
+    ['document.write("bookmarklet");', 'javascript:document.write(%22bookmarklet%22);'],
+    ['window.open("http://www.google.com");', 'javascript:window.open(%22http://www.google.com%22);'],
+    ['const getMessage = () => "Hello World";', 'javascript:const%20getMessage=()=%3E%22Hello%20World%22;'],
   ];
 
-  data.forEach( ( [ input, output ] ) => test.equal( bookmarkleter( input ), output ) );
+  data.map(([input, output]) => test.equal(bookmarkleter(input), output));
 
   test.done();
 };
